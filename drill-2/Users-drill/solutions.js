e5413f863fa6f7150c043db7a6dd627914bff7b1
Gopal Sable <sablegopal123@gmail.com>
2025-02-04 12:10:52 +0530

    add test cases in to drill-2

--- drill-2/Users-drill/solutions.js
+++ drill-2/Users-drill/solutions.js
@@ -1,53 +1,81 @@
 // Q1 Find all users who are interested in playing video games.
 function findInterested(users, hobby) {
+  if (typeof users != "object") {
+    return "Please provide a object";
+  }
+  if (hobby == null) {
+    return "Provide hobby";
+  }
   let userWithInterset = [];
   for (let key in users) {
     users[key].interests.forEach((interest) => {
       if (interest.includes(hobby)) {
-        userWithInterset.push(users[key]);
+        userWithInterset[key] = users[key];
       }
     });
   }
-  console.log(userWithInterset);
+  return userWithInterset;
 }
 
 // Q2 Find all users staying in Germany.
 function findByNationality(users, nation) {
+  if (typeof users != "object") {
+    return "Please provide a object";
+  }
+  if (nation == null) {
+    return "Provide nationality";
+  }
+
   let data = [];
   for (const user in users) {
     if (users[user].nationality == nation) {
-      data.push(users[user]);
+      data[user] = users[user];
     }
   }
   return data;
 }
 
-
 // Q3 Find all users with masters Degree.
-function findByQualification(users,qualification) {
-    let data = [];
-    for (const user in users) {
-      if (users[user].qualification == qualification) {
-        data.push(users[user]);
-      }
-    }
-    return data;
+function findByQualification(users, qualification) {
+  if (typeof users != "object") {
+    return "Please provide a object";
   }
-  
+
+  if (qualification == null) {
+    return "Provide qualification";
+  }
+  let data = [];
+  for (const user in users) {
+    if (users[user].qualification == qualification) {
+      data[user] = users[user];
+    }
+  }
+  return data;
+}
+
 // Q4 Group users based on their Programming language mentioned in their designation.
 function getGroupsByProgram(users) {
-    let data = {} //[["Golang"],["JavaScript"], ["Python"]];
-    for (const user in users) {
-      users[user].desgination.split(" ").map(language=>{
-        if (["Golang","Javascript", "Python"].includes(language)) {
-            if (!data[language]) {
-              data[language]=[]
-            }
-            data[language].push(users[user])
-        }
-      }) 
-    }
-    return data;
+  if (typeof users != "object" || Object.keys(users).length == 0) {
+    return "Please provide a object";
   }
 
-export { findInterested, findByNationality, findByQualification, getGroupsByProgram };
+  let data = {}; //[["Golang"],["JavaScript"], ["Python"]];
+  for (const user in users) {
+    users[user].desgination.split(" ").map((language) => {
+      if (["Golang", "Javascript", "Python"].includes(language)) {
+        if (!data[language]) {
+          data[language] = [];
+        }
+        data[language].push(users[user]);
+      }
+    });
+  }
+  return data;
+}
+
+export {
+  findInterested,
+  findByNationality,
+  findByQualification,
+  getGroupsByProgram,
+};
--- drill-2/Users-drill/tests.js
+++ drill-2/Users-drill/tests.js
@@ -1,19 +1,48 @@
 import users from "./users.js";
-import { findInterested, findByQualification, findByNationality } from "./solutions.js";
+import {
+  findInterested,
+  findByQualification,
+  findByNationality,
+  getGroupsByProgram,
+} from "./solutions.js";
 
 // Q1 Find all users who are interested in playing video games.
-findInterested(users,"Playing Video Games");
+console.log(
+  "\n\n\n============================ Problem 1 ============================"
+);
 
+console.log(findInterested(users, "Video Games"));
+console.log(findInterested());
+console.log(findInterested("sda"));
+console.log(findInterested(users));
 
+// // // Q2 Find all users staying in Germany.
 
-// Q2 Find all users staying in Germany.
-console.log(findByNationality(users,"Germany"));
+console.log(
+  "\n\n\n============================ Problem 2 ============================"
+);
+console.log(findByNationality(users, "Germany"));
+console.log(findByNationality(users));
+console.log(findByNationality());
+console.log(findByNationality("sda"));
+console.log(findByNationality(users, 123));
 
-  
-// Q3 Find all users with masters Degree.
+// // Q3 Find all users with masters Degree.
 
-console.log(findByQualification(users,"Masters"));
+console.log(
+  "\n\n\n============================ Problem 3 ============================"
+);
+console.log(findByQualification(users, "Masters"));
+console.log(findByQualification(users));
+console.log(findByQualification());
+console.log(findByQualification("sda"));
+console.log(findByQualification(users, 123));
 
+// // Q4 Group users based on their Programming language mentioned in their designation.
 
-// Q4 Group users based on their Programming language mentioned in their designation.
+console.log(
+  "\n\n\n============================ Problem 4 ============================"
+);
 console.log(getGroupsByProgram(users));
+console.log(getGroupsByProgram());
+console.log(getGroupsByProgram("sda"));
